36cc461b67bfe6a88c35bc73ce16c63c8485970f
Dan Leatherman <dleatherman@gmail.com>
2023-04-04 17:07:57 -0400

    remove users route

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var logger = require('morgan');
 const { WebCEngine } = require("express-webc");
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -21,7 +20,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/blog/', express.static(path.join(__dirname, 'blog')))
 
 module.exports = app;
